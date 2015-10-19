730bb269bdcd8273ba45223ce56506789bc7e033
msgal97 <klyoung2@mail.bradley.edu>
2015-10-19 09:34:30 -0500

    update console link

--- whale.js
+++ whale.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function($) {
   var num = parseInt(location.hash.substr(1));
   console.log(num);
   $.get('/whale/txt/' + num + '.txt', function(txt)) {
-    cosole.log(txt);
+    $('a').html(txt);
+    $('a').attr('href', '#' + (num +1));
+    //cosole.log(txt);
   }
   if (isNan(num)) {
     num = 0;
