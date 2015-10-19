8d75f3b1706f4f0c64f1f306d6322f84990023e2
msgal97 <klyoung2@mail.bradley.edu>
2015-10-19 09:27:38 -0500

    add ajax

--- whale.js
+++ whale.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function($) {
   var num = parseInt(location.hash.substr(1));
   console.log(num);
+  $.get('/whale/txt/' + num + '.txt', function(txt)) {
+    cosole.log(txt);
+  }
   if (isNan(num)) {
     num = 0;
   }
